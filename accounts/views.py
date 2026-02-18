15985585fe46651eefe59137ff2e92f255b60a05
kivancsy <kivancs.yildirim@gmail.com>
2026-02-18 23:52:50 +0300

    register/login added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 # Create your views here.
+
+def Register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('journal:home')
+    else:
+        form = UserCreationForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
