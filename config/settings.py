5a2c5d8885f6aa780ff5ff3d778ce59f6e0187cf
kivancsy <kivancs.yildirim@gmail.com>
2026-02-25 03:11:18 +0300

    settings.py updated

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Application definition
